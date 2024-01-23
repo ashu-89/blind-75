1474aa78ec59246717291e52a5931b67608f3bd5
Sakha Global <ashutosh.pathak@sakhaglobal.com>
2024-01-23 15:17:58 +0530

    wip or fixed, dunno, haaha, changing laptop so just pushing

---
+++ src/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public static int findMinInRotatedArray(int[] arr) {
+
+        // Replace this placeholder return statement with your code
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5,1,2,3,4,};
+    }
+}
